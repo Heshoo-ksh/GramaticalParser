8a6aae9e8b1e5236f8050d4371d41545d6555c60
Heshoo-ksh <hishamodeh2001@gmail.com>
2023-12-08 09:31:07 -0500

    use the NLTK library to verify that the base from of a word actually exisits

--- app/views.py
+++ app/views.py
@@ -3,9 +3,16 @@ from flask import request, jsonify
 import spacy
 from spellchecker import SpellChecker
 from app.models import NounObject
+import nltk
+
+
+nltk.download('words')
 
 nlp = spacy.load("en_core_web_sm")
 spell = SpellChecker()
+
+def is_valid_english_word(word):
+    return word.lower() in set(nltk.corpus.words.words())
 
 @app.route('/')
 def hello_world():
@@ -27,8 +34,8 @@ def parse_story():
             break
 
     if main_noun_obj:
-        compound_nouns = set()  # To track compound nouns
-        individual_nouns = set()  # To track individual nouns
+        compound_nouns = set() 
+        individual_nouns = set() 
 
         # First pass: Identify and store compound nouns
         for token in doc:
@@ -38,18 +45,18 @@ def parse_story():
 
         # Second pass: Collect associations avoiding duplicates
         for token in doc:
+            # Check for direct and indirect associations with the main noun
             if token.pos_ == 'VERB' and (token.head.text == main_noun_obj.noun or main_noun_obj.noun in [ancestor.text for ancestor in token.ancestors]):
                 verb_lemma = token.lemma_
-                # Use SpaCy's vocabulary to check if lemma is a valid English word
-                if verb_lemma in nlp.vocab:
-                    main_noun_obj.add_associated_verb(token.lemma_)
+                # Validate lemma using NLTK's English word list
+                if is_valid_english_word(verb_lemma):
+                    main_noun_obj.add_associated_verb(verb_lemma)
                 else:
                     main_noun_obj.add_associated_verb(token.text)
             elif token.pos_ == 'NOUN' and token.text.lower() != main_noun_obj.noun.lower():
                 if not any(token.text.lower() in cn for cn in compound_nouns):
                     individual_nouns.add(token.text.lower())
 
-        # Add compound nouns and individual nouns to the associations
         for cn in compound_nouns:
             main_noun_obj.add_associated_noun(cn)
         for noun in individual_nouns:
