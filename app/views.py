9a49444a59723b10af190a4a8bac8b8d5505d6b6
Heshoo-ksh <hishamodeh2001@gmail.com>
2023-12-08 07:36:06 -0500

    using base form for verbs

--- app/views.py
+++ app/views.py
@@ -27,30 +27,34 @@ def parse_story():
             break
 
     if main_noun_obj:
+        compound_nouns = set()  # To track compound nouns
+        individual_nouns = set()  # To track individual nouns
+
+        # First pass: Identify and store compound nouns
         for token in doc:
-            # Check for direct and indirect associations with the main noun
-            if token.head.text == main_noun_obj.noun or main_noun_obj.noun in [ancestor.text for ancestor in token.ancestors]:
-                if token.pos_ == 'VERB':
-                    main_noun_obj.add_associated_verb(token.text)
-                elif token.pos_ == 'NOUN' and token.text.lower() != main_noun_obj.noun.lower():
-                    # Check for compound nouns
-                    if token.dep_ == 'compound':
-                        compound_noun = token.text + ' ' + token.head.text
-                        main_noun_obj.add_associated_noun(compound_noun)
-                    else:
-                        main_noun_obj.add_associated_noun(token.text)
+            if token.dep_ == 'compound' and token.head.pos_ == 'NOUN':
+                compound_noun = token.text + ' ' + token.head.text
+                compound_nouns.add(compound_noun.lower())
+
+        # Second pass: Collect associations avoiding duplicates
+        for token in doc:
+            if token.pos_ == 'VERB' and (token.head.text == main_noun_obj.noun or main_noun_obj.noun in [ancestor.text for ancestor in token.ancestors]):
+                main_noun_obj.add_associated_verb(token.lemma_)
+            elif token.pos_ == 'NOUN' and token.text.lower() != main_noun_obj.noun.lower():
+                if not any(token.text.lower() in cn for cn in compound_nouns):
+                    individual_nouns.add(token.text.lower())
+
+        # Add compound nouns and individual nouns to the associations
+        for cn in compound_nouns:
+            main_noun_obj.add_associated_noun(cn)
+        for noun in individual_nouns:
+            main_noun_obj.add_associated_noun(noun)
 
     response = {
         "nouns": [main_noun_obj.to_dictionary()] if main_noun_obj else []
     }
     return jsonify(response)
-
-
-
-
-
-
-    
+  
 
 @app.route('/spell_check', methods=['POST'])
 def spell_check():
